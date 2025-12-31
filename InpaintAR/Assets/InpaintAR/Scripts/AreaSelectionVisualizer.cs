ca5e4efc2faa91e078c4c3594054cf4bda47c2bd
Sebastian Gradwohl <sebiplay15@gmx.at>
2025-12-31 13:47:36 +0100

    Selected Area now gets cropped correctly

--- InpaintAR/Assets/InpaintAR/Scripts/AreaSelectionVisualizer.cs
+++ InpaintAR/Assets/InpaintAR/Scripts/AreaSelectionVisualizer.cs
@@ -10,29 +10,32 @@ namespace InpaintAR.Scripts {
 
         [Tooltip("Size of the corner sprites image")]
         public int cornerSpriteSize = 128;
+
         [Tooltip("Thickness of the corner sprite lines")]
         public int cornerSpriteThickness = 50;
-        
-        public RectTransform FillRect { get; private set; }
+
+        public RectTransform FillRectMask { get; private set; }
 
         // Internal references to the generated UI elements
         private RectTransform m_leftCornerBox;
         private RectTransform m_rightCornerBox;
         private Canvas m_canvas;
-        
-        private RawImage FillArea { get; set; }
-        
-        [Header("Debug Data")]
+
+        private RawImage FillImage { get; set; }
+
+        [Header("Debug Settings")] 
+        [Tooltip("Uses a solid red box instead of the camera texture for display")]
         public bool showDebugRect = true;
 
         private Ray m_textureTopLeft;
         private Ray m_textureBottomRight;
         private PassthroughCameraAccess m_cameraAccess;
+        private RectTransform m_fillImageRect; // rect transform of the child RawImage
 
         private void Start() {
             if (!areaDetection) {
-               Debug.LogError("AreaSelectionVisualizer: AreaDetection not set!");
-               return;
+                Debug.LogError("AreaSelectionVisualizer: AreaDetection not set!");
+                return;
             }
 
             CreateUICanvasAndCorners();
@@ -43,8 +46,8 @@ namespace InpaintAR.Scripts {
         }
 
         void Update() {
-            if (   !areaDetection 
-                || !m_leftCornerBox 
+            if (!areaDetection
+                || !m_leftCornerBox
                 || !m_rightCornerBox) return;
 
             UpdateCorner(
@@ -58,45 +61,72 @@ namespace InpaintAR.Scripts {
             );
 
             // only update if rectangle is valid
-            if (   areaDetection.LeftHandCornerScreenPos.HasValue 
+            if (areaDetection.LeftHandCornerScreenPos.HasValue
                 && areaDetection.RightHandCornerScreenPos.HasValue
                 && areaDetection.LeftHandCornerScreenPos.Value.x <= areaDetection.RightHandCornerScreenPos.Value.x
-                && areaDetection.LeftHandCornerScreenPos.Value.y >= areaDetection.RightHandCornerScreenPos.Value.y)  {
-                FillRect.gameObject.SetActive(true);
+                && areaDetection.LeftHandCornerScreenPos.Value.y >= areaDetection.RightHandCornerScreenPos.Value.y) {
+                FillRectMask.gameObject.SetActive(true);
 
+                // Image position calculation so it matches with the passthrough background as close as possible
                 m_textureTopLeft = m_cameraAccess.ViewportPointToRay(new Vector2(0, 0));
                 m_textureBottomRight = m_cameraAccess.ViewportPointToRay(new Vector2(1, 1));
-                
-                // Convert ray origins of passthrough camera to screen space and then to local UI coordinates of actual main camera
+
                 Vector3 topLeftWorldPos = m_textureTopLeft.origin + m_textureTopLeft.direction * (m_canvas.worldCamera.farClipPlane - 0.1f);
                 Vector3 bottomRightWorldPos = m_textureBottomRight.origin + m_textureBottomRight.direction * (m_canvas.worldCamera.farClipPlane - 0.1f);
                 Vector3 topLeftScreenPos = m_canvas.worldCamera.WorldToScreenPoint(topLeftWorldPos);
                 Vector3 bottomRightScreenPos = m_canvas.worldCamera.WorldToScreenPoint(bottomRightWorldPos);
-                
+
                 RectTransformUtility.ScreenPointToLocalPointInRectangle(
                     (RectTransform)m_canvas.transform,
                     topLeftScreenPos,
                     m_canvas.worldCamera,
                     out Vector2 topLeftLocalPos
                 );
-                
+
                 RectTransformUtility.ScreenPointToLocalPointInRectangle(
                     (RectTransform)m_canvas.transform,
                     bottomRightScreenPos,
                     m_canvas.worldCamera,
                     out Vector2 bottomRightLocalPos
                 );
-                
+
                 Vector2 bottomLeft = new Vector2(Mathf.Min(topLeftLocalPos.x, bottomRightLocalPos.x), Mathf.Min(topLeftLocalPos.y, bottomRightLocalPos.y));
                 Vector2 topRight = new Vector2(Mathf.Max(topLeftLocalPos.x, bottomRightLocalPos.x), Mathf.Max(topLeftLocalPos.y, bottomRightLocalPos.y));
+
+                // Selection Rectangle Calculation
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    (RectTransform)m_canvas.transform,
+                    areaDetection.LeftHandCornerScreenPos.Value,
+                    m_canvas.worldCamera,
+                    out Vector2 selLeftLocal
+                );
+
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    (RectTransform)m_canvas.transform,
+                    areaDetection.RightHandCornerScreenPos.Value,
+                    m_canvas.worldCamera,
+                    out Vector2 selRightLocal
+                );
+
+                Vector2 selectionBottomLeft = new Vector2(Mathf.Min(selLeftLocal.x, selRightLocal.x),
+                    Mathf.Min(selLeftLocal.y, selRightLocal.y));
+                Vector2 selectionTopRight = new Vector2(Mathf.Max(selLeftLocal.x, selRightLocal.x),
+                    Mathf.Max(selLeftLocal.y, selRightLocal.y));
+
+                // Mask Update
+                FillRectMask.localPosition = selectionBottomLeft;
+                FillRectMask.sizeDelta = selectionTopRight - selectionBottomLeft;
                 
-                FillArea.texture = m_cameraAccess.GetTexture();
-                FillArea.SetNativeSize();
-                FillRect.localPosition = bottomLeft;
-                FillRect.sizeDelta = topRight - bottomLeft;
+                if (!showDebugRect) {
+                    FillImage.texture = m_cameraAccess.GetTexture();
+                }
+
+                // Readjust Child Image Position to adjust for FillRect Mask Update
+                m_fillImageRect.localPosition = bottomLeft - selectionBottomLeft;
+                m_fillImageRect.sizeDelta = topRight - bottomLeft;
             }
             else {
-                FillRect.gameObject.SetActive(false);
+                FillRectMask.gameObject.SetActive(false);
             }
         }
 
@@ -117,7 +147,7 @@ namespace InpaintAR.Scripts {
 
             cornerElem.localPosition = localPos;
         }
-        
+
         private void CreateUICanvasAndCorners() {
             GameObject canvasObj = new GameObject("AreaSelectionUICanvas");
             m_canvas = canvasObj.AddComponent<Canvas>();
@@ -125,7 +155,7 @@ namespace InpaintAR.Scripts {
             m_canvas.worldCamera = Camera.main;
             // place at far clipping pane -> no virtual objects are hidden
             m_canvas.planeDistance = Camera.main.farClipPlane - 0.1f;
-            
+
             CanvasScaler scaler = canvasObj.AddComponent<CanvasScaler>();
             scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
             scaler.referenceResolution = new Vector2(1920, 1080);
@@ -145,23 +175,33 @@ namespace InpaintAR.Scripts {
             m_rightCornerBox = rightObj.AddComponent<RectTransform>();
             SetupCornerImage(m_rightCornerBox, false);
             rightObj.SetActive(false);
-            
-            // Create the fill area game object
-            GameObject fillObj = new GameObject("FillArea");
-            fillObj.transform.SetParent(canvasObj.transform, false);
-            FillRect = fillObj.AddComponent<RectTransform>();
-            
-            FillRect.pivot = new Vector2(0, 0);
-            FillRect.anchorMin = FillRect.anchorMax = new Vector2(0.5f, 0.5f);
 
-            // Add an Image to fill it with red initially
-            FillArea = fillObj.AddComponent<RawImage>();
-            FillArea.color = showDebugRect ? Color.red : Color.white;
+            // Mask Container
+            GameObject fillObj = new GameObject("MaskContainer");
+            fillObj.transform.SetParent(canvasObj.transform, false);
+            FillRectMask = fillObj.AddComponent<RectTransform>();
+
+            FillRectMask.pivot = new Vector2(0, 0);
+            FillRectMask.anchorMin = FillRectMask.anchorMax = new Vector2(0.5f, 0.5f);
+
+            // Add a RectMask2D so children are clipped to this rectangle (window behavior)
+            fillObj.AddComponent<RectMask2D>();
+
+            // Image Child Item -> RectTransform to allow for individual placement separate from FillRect Mask
+            GameObject fillImageObj = new GameObject("FillImage");
+            fillImageObj.transform.SetParent(fillObj.transform, false);
+            m_fillImageRect = fillImageObj.AddComponent<RectTransform>();
+            m_fillImageRect.anchorMin = m_fillImageRect.anchorMax = new Vector2(0, 0);
+            m_fillImageRect.pivot = new Vector2(0, 0);
+
+            // Add a RawImage for passthrough texture
+            FillImage = fillImageObj.AddComponent<RawImage>();
+            FillImage.color = showDebugRect ? Color.red : Color.white;
 
             // Start hidden
-            FillRect.gameObject.SetActive(false);
+            FillRectMask.gameObject.SetActive(false);
         }
-        
+
         private void SetupCornerImage(RectTransform target, bool isLeft) {
             var img = target.GetComponent<Image>();
             if (!img) {
@@ -174,7 +214,7 @@ namespace InpaintAR.Scripts {
 
             float anchorX = isLeft ? 0 : 1;
             float anchorY = isLeft ? 1 : 0;
-            
+
             target.anchorMin = target.anchorMax = new Vector2(anchorX, anchorY); // center canvas to camera
             target.pivot = new Vector2(anchorX, anchorY); // center reference to image center
             target.sizeDelta = new Vector2(cornerSpriteSize, cornerSpriteSize);
@@ -189,8 +229,9 @@ namespace InpaintAR.Scripts {
             for (int i = 0; i < pixels.Length; i++) {
                 pixels[i] = Color.clear;
             }
+
             texture.SetPixels(pixels);
-            
+
             // Horizontal line
             int yStart = isLeft ? size - thickness : 0;
             int yEnd = isLeft ? size : thickness;
